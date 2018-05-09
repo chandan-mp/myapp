d3d042f5eaef8d94d1887bc9e66cedf0634d3d03
chandanswain <chandan.swain@mobileprogramming.com>
2018-05-09 13:24:54 +0530

    project deployed in git

---
+++ routes/users.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+
+router.get('/user', function(req, res, next) {
+    res.send(JSON.stringify({
+        "name":"Chandan",
+        "Email": "chandan.swain@mobileprogramming"
+    }));
+});
+
+module.exports = router;
